9d69262f3482b7f83f65d3b8e6c3d4e6a0d9900c
Richard <71474562+RichardOlaniyan@users.noreply.github.com>
2024-04-20 00:00:28 -0500

    Add a location icon

--- Start/my-real-card-app/src/Card.jsx
+++ Start/my-real-card-app/src/Card.jsx
@@ -1,4 +1,5 @@
 import './Card-styles.css'
+import navigationicon from './assets/navigationicon.svg'
 
 function Card(props){
     return(
@@ -12,7 +13,10 @@ function Card(props){
             (< Placeholder first={props.user.name.first} last={props.user.name.first}/>)
         }
             <h2 className='user-card-name'>{`${props.user.name.first} ${props.user.name.last}`}</h2>
-            <p className='user-card-location'>{`${props.user.location.country}`}</p>
+            <div className='location-wrap'>
+            <img className='user-location-icon'src={navigationicon}></img>
+            <p className='user-card-location'>{`${props.user.location.city}`}</p>
+            </div>
             <a className='user-card-number' src={props.user.email}>{`Contact ${props.user.name.first}`}</a>
         </div>
     );
